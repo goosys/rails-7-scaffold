badd72072f8f44d261de9a729d5556294b46e408
t.nakamura <shop@goosys.sakura.ne.jp>
2022-12-03 23:05:38 +0900

    WIP: add build settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  #
+  config.hosts << URI.parse("https://#{ENV['APP_HOST']}").host
+  config.hosts << IPAddr.new(ENV.fetch('VPC_CIDR', "172.30.0.0/16"))
 end
